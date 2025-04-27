a8f0c723e4d02701091cf211adb651859d5740c9
Midhun Shyam <164188456+midhunshyam@users.noreply.github.com>
2025-04-27 15:24:01 +1000

    source code

--- Scripts/Bio_ClinicalBERTClassifier.py
+++ Scripts/Bio_ClinicalBERTClassifier.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Feb 26 23:54:06 2025
 @model: Bio_ClinicalBERTClassifier
-author: Midhun Shyam (M.Shyam)
+author: Midhun Shyam & Dr. X. Rosalind Wang (Research Supervisor)
 """
 
 import os
