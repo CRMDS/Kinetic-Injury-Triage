e5cdb6682e5fff1546d41c7b95123f471b21120e
Rosalind Wang <rosalind@rosalindwang.net>
2025-06-10 11:08:39 +1000

    update script to also extract exitcode from each task.

--- Nextflow/gadi_nf_extract_tasks.sh
+++ Nextflow/gadi_nf_extract_tasks.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Script to extract 
+# -- task information from .command.run files 
+# -- exit code from the .exitcode file
+# in a Nextflow workflow directory, and save them to csv files. 
+
+
+#--------- TASK EXTRACTION ---------#
 
 # Output CSV file
 output_file="gadi-nf-tasks.tsv"
@@ -21,5 +28,30 @@ find work -type f -name ".command.run" | while read -r log_file; do
 
 done
 
-echo "Done. Output saved to $output_file"
+echo "Output saved to $output_file"
+
+
+#--------- EXIT CODE EXTRACTION ---------#
+
+# Output CSV file for exit codes
+exit_code_file="gadi-nf-exitcodes.tsv"
+
+# Write CSV header
+echo -e "file_path\texit_code" > "$exit_code_file"
+
+# Find all .exitcode files and loop through them
+find work -type f -name ".exitcode" | while read -r exit_file; do
+
+    # Extract the exit code from the file
+    exit_code=$(cat "$exit_file")   
+
+    # Write the file path and exit code to the output file
+    echo -e "$exit_file\t$exit_code" >> "$exit_code_file"
+
+done
+
+echo "Exit codes saved to $exit_code_file"
+
+echo "All tasks and exit codes have been extracted successfully."
+
 
